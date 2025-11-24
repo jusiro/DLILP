9cb1964b2e483fedc5882f7d7fe9aa5fd258b715
Julio Silva <jusiro95@gmail.com>
2025-11-24 16:10:07 +0100

    Modularize DLILP

--- cxrvlms/__init__.py
+++ cxrvlms/__init__.py
@@ -1,1 +1,6 @@
+name = "dlilp",
+version = "0.1.0",
+
 from .modeling.model import VLMModel
+from .modeling.constants import CATEGORIES
+from .modeling.prompts import generate_prompt_ensemble, CATEGORIES_ALL, ASSEMBLE_PROMPTS
